9505039a3feba6db83bbfb3df64bcce8b6b488b3
Allan Bua <61344515+allan-bua-okello@users.noreply.github.com>
2024-08-20 16:30:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let sum=0;
+	const lists=new Array();
+	let res=new Array();
+	arr.forEach(a=>
+		{
+			if(sum+a<=n)
+			{
+				res.push(a);
+				sum+=a;
+			}
+			else
+			{
+				lists.push(res);
+				res=new Array();
+				res.push(a);
+				sum=a;
+				
+			}
+		});
+	lists.push(res);
+	return lists;
 };
 
 const n = prompt("Enter n: ");
